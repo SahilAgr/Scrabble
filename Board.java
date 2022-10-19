0e265ee02aa7aeb0d52e499cf3f06636f49d7262
Matthew <matthewhuitema2000@gmail.com>
2022-10-18 22:22:46 -0400

    adding the enums

--- Board.java
+++ Board.java
@@ -1,2 +1,9 @@
 public class Board {
+    //enums for the x and y co-ords. enums doesnt like it when i use integers.
+    private enum yCoordinate{ONE,TWO,THREE,FOUR,FIVE,SIX,
+        SEVEN,EIGHT,NINE,TEN,ELEVEN,TWELVE,THIRTEEN,FOURTEEN,FIFTEEN;}
+    private enum xCoordinate{A,B,C,D,E,F,G,H,I,J,K,L,M,N,O;}
+    public Board(){
+
+    }
 }
--- Game.java
+++ Game.java
@@ -1,8 +1,25 @@
 import java.util.List;
+import java.util.Map;
 
 public class Game {
     private List<Player> players;
-    private WordDictonary dictonary;
+    private Dictionary dictionary;
     private Board board;
     private Parser parser;
+    private Map<letter,Integer> letterScores;
+
+    //hardcoding the letters so we dont get reliant on strings...
+    //not that a single letter is likely to create a typo... but hey
+    private enum letter{
+        A,B,C,D,E,F,G,H,I,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
+    }
+
+    public Game(){
+        /**create letterbag with setup peices in place (xAs, yBs, etc)
+        foreach letter in letterbag, assign a score.
+        im halfassed designing while im tired, so im not sure where
+        the most benifical placement of the enum letters is.
+        (probably in letterbag TBH.)
+        **/      
+    }   
 }
