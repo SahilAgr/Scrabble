a48f3630a1ba77b54173a12ab367a466051cae07
Matthew <matthewhuitema2000@gmail.com>
2022-10-23 13:58:40 -0400

    letter enum

--- Player.java
+++ Player.java
@@ -2,48 +2,53 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Player {
-    private Integer score;
-    private ArrayList<letter> Hand;
-    private HashMap<letter, Integer> letterScores;
     
-    private enum letter{
+    Integer score;
+
+    private enum Letter{
         A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
+
+        private HashMap<Letter, Integer> letterScores;
+        private Letter(){
+            letterScores = new HashMap<>();
+            createLetterScores();
+        }
+
+        private void createLetterScores() {
+            letterScores.put(Letter.A, 1);
+            letterScores.put(Letter.B, 3);
+            letterScores.put(Letter.C, 3);
+            letterScores.put(Letter.D, 2);
+            letterScores.put(Letter.E, 1);
+            letterScores.put(Letter.F, 4);
+            letterScores.put(Letter.G, 2);
+            letterScores.put(Letter.H, 4);
+            letterScores.put(Letter.I, 1);
+            letterScores.put(Letter.J, 8);
+            letterScores.put(Letter.K, 5);
+            letterScores.put(Letter.L, 1);
+            letterScores.put(Letter.M, 3);
+            letterScores.put(Letter.N, 1);
+            letterScores.put(Letter.O, 1);
+            letterScores.put(Letter.P, 3);
+            letterScores.put(Letter.Q, 10);
+            letterScores.put(Letter.R, 1);
+            letterScores.put(Letter.S, 1);
+            letterScores.put(Letter.T, 1);
+            letterScores.put(Letter.U, 1);
+            letterScores.put(Letter.V, 4);
+            letterScores.put(Letter.W, 4);
+            letterScores.put(Letter.X, 8);
+            letterScores.put(Letter.Y, 4);
+            letterScores.put(Letter.Z, 10);
+        }
+
+        public Integer getLetterScore(Letter letter){
+            return letterScores.get(letter);
+        }
     }
     
     public Player(){
-        letterScores = new HashMap();
-        createLetterScores();
         score = 0;
-    }
-
-
-    //Yes, it's disguisting.
-    private void createLetterScores() {
-        letterScores.put(letter.A, 1);
-        letterScores.put(letter.B, 3);
-        letterScores.put(letter.C, 3);
-        letterScores.put(letter.D, 2);
-        letterScores.put(letter.E, 1);
-        letterScores.put(letter.F, 4);
-        letterScores.put(letter.G, 2);
-        letterScores.put(letter.H, 4);
-        letterScores.put(letter.I, 1);
-        letterScores.put(letter.J, 8);
-        letterScores.put(letter.K, 5);
-        letterScores.put(letter.L, 1);
-        letterScores.put(letter.M, 3);
-        letterScores.put(letter.N, 1);
-        letterScores.put(letter.O, 1);
-        letterScores.put(letter.P, 3);
-        letterScores.put(letter.Q, 10);
-        letterScores.put(letter.R, 1);
-        letterScores.put(letter.S, 1);
-        letterScores.put(letter.T, 1);
-        letterScores.put(letter.U, 1);
-        letterScores.put(letter.V, 4);
-        letterScores.put(letter.W, 4);
-        letterScores.put(letter.X, 8);
-        letterScores.put(letter.Y, 4);
-        letterScores.put(letter.Z, 10);
     }
 }
