530f085a6fa0ef25faa8da2bca0e9fc794d7d555
Sahil Agrawal <sahilagr21@gmail.com>
2022-11-11 13:53:03 -0500

    wrapping up some testing

--- CoordinatesTest.java
+++ CoordinatesTest.java
@@ -4,16 +4,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CoordinatesTest {
+    Coordinates coordinates;
 
     @BeforeEach
     void setUp() {
+        this.coordinates = new Coordinates(Coordinates.xCoordinate.A, Coordinates.yCoordinate.EIGHT);
     }
 
     @Test
     void getXCoordinate() {
+
+        assertEquals(Coordinates.xCoordinate.A,coordinates.getXCoordinate());
     }
 
     @Test
     void getYCoordinate() {
+        assertEquals(Coordinates.yCoordinate.EIGHT,coordinates.getYCoordinate());
     }
 }
