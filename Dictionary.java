5edd1f2b1caf404d103527fbfd8fbb1f66ba4fbc
Anirudh300 <66482608+Anirudh300@users.noreply.github.com>
2022-12-06 22:58:38 -0500

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -8,7 +8,9 @@ import java.util.List;
  * @author Anirudh Bakshi (101158699)
  * @version 2.0
  */
-public class Dictionary {
+public class Dictionary implements Serializable {
+    
+    public static final long serialVersionUID = 1L;
 
 
     /**
